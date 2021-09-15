0e548a862d1f459186ce0940783b508b11369da4
b <a@gmail.com>
2021-09-14 17:23:13 -0700

    sbtp

---
+++ mysbtest/src/main/java/com/learning/mysbtest/mysbtest/MysbtestApplication.java
@@ -0,0 +1,13 @@
+package com.learning.mysbtest.mysbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysbtestApplication.class, args);
+    }
+
+}
---
+++ mysbtest/src/test/java/com/learning/mysbtest/mysbtest/MysbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.mysbtest.mysbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
